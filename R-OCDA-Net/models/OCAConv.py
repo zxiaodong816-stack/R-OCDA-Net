53dab357085a570865fa5f82818ea061153b6041
Vegetable <zxiaodong816@gmail.com>
2026-07-17 13:14:52 +0800

    Update OCAConv.py

--- R-OCDA-Net/models/OCAConv.py
+++ R-OCDA-Net/models/OCAConv.py
@@ -1,1 +1,78 @@
-"""TODO: Add OCAConv implementation."""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ultralytics.nn.modules.conv import Conv,autopad
+from ultralytics.nn.modules.block import Bottleneck, C2f,C3k,C3k2
+
+
+class OCAConv(nn.Module):
+    ''' Pinwheel-shaped Convolution using the Asymmetric Padding method. '''
+    
+    def __init__(self, c1, c2, k, s):
+        super().__init__()
+
+        # self.k = k
+        p = [(k, 0, 1, 0), (0, k, 0, 1), (0, 1, k, 0), (1, 0, 0, k)]
+        self.pad = [nn.ZeroPad2d(padding=(p[g])) for g in range(4)]
+        self.cw = Conv(c1, c2 // 4, (1, k), s=s, p=0)
+        self.ch = Conv(c1, c2 // 4, (k, 1), s=s, p=0)
+        self.cat = Conv(c2, c2, 2, s=1, p=0)
+
+    def forward(self, x):
+        yw0 = self.cw(self.pad[0](x))
+        yw1 = self.cw(self.pad[1](x))
+        yh0 = self.ch(self.pad[2](x))
+        yh1 = self.ch(self.pad[3](x))
+        return self.cat(torch.cat([yw0, yw1, yh0, yh1], dim=1))
+
+
+class APC2f(nn.Module):
+    """Faster Implementation of APCSP Bottleneck with Asymmetric Padding convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, P=True, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        if P:
+            self.m = nn.ModuleList(APBottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+        else:
+            self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+
+class APBottleneck(nn.Module):
+    """Asymmetric Padding bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a bottleneck module with given input/output channels, shortcut option, group, kernels, and
+        expansion.
+        """
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        p = [(2,0,2,0),(0,2,0,2),(0,2,2,0),(2,0,0,2)]
+        self.pad = [nn.ZeroPad2d(padding=(p[g])) for g in range(4)]
+        self.cv1 = Conv(c1, c_ // 4, k[0], 1, p=0)
+        # self.cv1 = nn.ModuleList([nn.Conv2d(c1, c_, k[0], stride=1, padding= p[g], bias=False) for g in range(4)])
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        """'forward()' applies the YOLO FPN to input data."""
+        # y = self.pad[g](x) for g in range(4)
+        return x + self.cv2((torch.cat([self.cv1(self.pad[g](x)) for g in range(4)], 1))) if self.add else self.cv2((torch.cat([self.cv1(self.pad[g](x)) for g in range(4)], 1)))
+
+
+class C3k_AP(C3k):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5, k=3):
+        super().__init__(c1, c2, n, shortcut, g, e, k)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(APBottleneck(c_,c_) for _ in range(n)))
+
+class C3k2_AP(C3k2):
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        super().__init__(c1, c2, n, c3k, e, g, shortcut)
+        self.m = nn.ModuleList(C3k_AP(self.c, self.c, 2, shortcut, g) if c3k else APBottleneck(self.c,self.c) for _ in range(n))
